c5fe5baba9dfd826efc3a0bf4281f19d1bcda3d2
menyawa <41637976+menyawa@users.noreply.github.com>
2020-01-05 16:32:06 +0900

    Fix Runner Image Size

--- Runner.cpp
+++ Runner.cpp
@@ -4,11 +4,8 @@
 Runner::Runner(int groundPosY) {
 	imageHandle_ = LoadGraph("./Image/Runner.png");
 	GetGraphSize(imageHandle_, &imageSizeX_, &imageSizeY_);
-	imageSizeX_ /= 2;
-	imageSizeY_ /= 2;
 
 	imagePosX_ = 0;
 	imagePosY_ = groundPosY - imageSizeY_;
-	//DrawGraph(imagePosX_, imagePosY_, imageHandle_, TRUE);
-	DrawExtendGraph(imagePosX_, imagePosY_, imageSizeX_, imageSizeY_, imageHandle_, TRUE);
+	DrawGraph(imagePosX_, imagePosY_, imageHandle_, TRUE);
 }
