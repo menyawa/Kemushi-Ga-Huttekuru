a3580cf1093d7f824ab68cc7cded1d6cf09b6e40
menyawa <41637976+menyawa@users.noreply.github.com>
2020-04-03 22:25:13 +0900

    Update Score Text Color

--- PlayerScore.cpp
+++ PlayerScore.cpp
@@ -8,6 +8,6 @@ PlayerScore::PlayerScore() {
 void  PlayerScore::printScore() {
 	const int xPos = 1000;
 	const int yPos = 25;
-	const int color = GetColor(0, 0, 0);
+	const int color = GetColor(255, 255, 255);
 	DrawFormatStringToHandle(xPos, yPos, color, fontHandle_, "スコア：%d", score_);
 }
